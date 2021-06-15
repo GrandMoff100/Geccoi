eaacd8c6a199818b8c71a98ad856b0218178b037
Zeb Taylor <zceboys@gmail.com>
2021-06-15 16:11:48 -0500

    Moved basic hand detection out of hand_tracking.py

--- geccoi/gesture_recog/hand_tracking/hand_tracking.py
+++ geccoi/gesture_recog/hand_tracking/hand_tracking.py
@@ -1,30 +1,8 @@
 import cv2
-import mediapipe as mp
+from ..detector import Detector
 
 
-class HandDetector:
-    def __init__(self, mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5):
-        self.mode = mode
-        self.max_num_hands = max_num_hands
-        self.min_detection_confidence = min_detection_confidence
-        self.min_tracking_confidence = min_tracking_confidence
-
-        self.hands = self.mp.solutions.hands.Hands(self.mode, self.max_num_hands,
-                                                   self.min_detection_confidence, self.min_tracking_confidence)
-        self.drawing_utils = mp.solutions.drawing_utils
-
-    def find_hands(self, img, draw=False):
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        self.results = self.hands.process(img)
-
-        if self.results.multi_hand_landmarks:
-            for hand in self.results.multi_hand_landmarks:
-                if draw:
-                    self.drawing_utils.draw_landmarks(img, hand, mp.solutions.hands.HAND_CONNECTIONS)
-
-        return img
-
+class HandDetector(Detector):
     def find_position(self, img, list=True, point=None, hand_num=0, draw=False):
         if list:
             landmark_list = []
